d162a23bd5d08bb38c780cac42d1e9f0a948c0a1
gupta123456 <anujg0046@gmail.com>
2020-12-23 21:54:02 +0530

    image-upload fixxed

--- server/services/cloudinary.js
+++ server/services/cloudinary.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary').v2;
 const config = require('../config');
 
 cloudinary.config({ 
-  cloud_name: config.CLOUDINARY_NAME, 
-  api_key: config.CLOUDINARY_KEY, 
-  api_secret: config.CLOUDINARY_SECRET 
+  cloud_name: 'dltwobcyu', 
+  api_key: '145719238317824', 
+  api_secret: 'PzLkvdowWPianMHUrrSPaMDZT-Q'
 });
 
 exports.cloudUpload = file => cloudinary.uploader.upload(file)
